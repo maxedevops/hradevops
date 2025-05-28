ea34ec0177cb882bf715b346b4f4040401bf14ca
Max <maxedevops@gmail.com>
2025-05-28 00:28:41 -0500

    Fix Terraform for GitHub Actions 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,3 +66,24 @@ resource "aws_ecs_service" "hra_service" {
     assign_public_ip = true
   }
 }
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Effect = "Allow"
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
